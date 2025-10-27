ac6bc03030178e599eb3eb213f25ae9d340aa222
陳筠臻 <yun@d-i89-200-222.student.eduroam.uq.edu.au>
2025-10-27 16:30:34 +1000

    add augmentation

--- recognition/siamese-s4852556/dataset.py
+++ recognition/siamese-s4852556/dataset.py
@@ -18,6 +18,15 @@ default_transform = transforms.Compose([
                          std=[0.229, 0.224, 0.225])
 ])
 
+# augmentation
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.RandomRotation(15),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
 
 # build image_paths and labels
 def build_image_label_list(img_dir=IMG_DIR, gt_csv=GT_CSV):
